26cf6ab623e6a49988927035bf8c7c076482a802
Michael Serpieri <michael.serpieri@pj.ca>
2018-01-09 15:23:05 -0500

    Bump version: 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atlasbroker',
-    version='1.0.0',
+    version='1.1.0',
     python_requires='>=3.5',
     packages=find_packages(),
     install_requires=['flask', 'openbrokerapi', 'pymongo', 'pwgen', 'atlasapi'],
